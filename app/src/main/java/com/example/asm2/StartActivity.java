035a834e09105a711737e040c83f64b80fa1b5a8
hahahalouis <louis.swhps61@gmail.com>
2022-04-02 21:28:05 +0800

    Asm2

---
+++ app/src/main/java/com/example/asm2/StartActivity.java
@@ -0,0 +1,36 @@
+package com.example.asm2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Thread thread = new Thread()
+        {
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }finally {
+                    Intent mainIntent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
