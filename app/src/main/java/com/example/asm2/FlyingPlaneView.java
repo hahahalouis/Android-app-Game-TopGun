3b76acb0f27c22a591ffe83897e7e7ee15f48f5f
hahahalouis <louis.swhps61@gmail.com>
2022-04-03 23:20:57 +0800

    Asm2

--- app/src/main/java/com/example/asm2/FlyingPlaneView.java
+++ app/src/main/java/com/example/asm2/FlyingPlaneView.java
@@ -4,24 +4,72 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class FlyingPlaneView extends View {
 
-    private Bitmap plane, bg;
+    private Bitmap plane, bg,resize_bg,resize_plane;
+    private int planeX = 10, planeY,planeSpeed;
+    private int canvasWidth, canvasHeight;
+    private boolean touchStatus = false;
+
 
     public FlyingPlaneView(Context context) {
         super(context);
         plane = BitmapFactory.decodeResource(getResources(), R.drawable.plane);
-        bg = BitmapFactory.decodeResource(getResources(),R.drawable.ingame_bg);
+        resize_plane = Bitmap.createScaledBitmap(plane, 200, 200 ,false);
+        planeY = 550;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawBitmap(bg,0,0,null);
-        canvas.drawBitmap(plane,0,0,null);
+        canvasHeight = canvas.getHeight();
+        canvasWidth = canvas.getWidth();
 
+        bg = BitmapFactory.decodeResource(getResources(),R.drawable.ingame_bg);
+        resize_bg = Bitmap.createScaledBitmap(bg, canvas.getWidth(),canvas.getHeight(),false);
+        canvas.drawBitmap(resize_bg,0,0,null);
+
+        int minPlaneY = resize_plane.getHeight();
+        int maxPlaneY = canvasHeight - resize_plane.getHeight() * 3;
+        planeY = planeY + planeSpeed;
+
+        if(planeY < minPlaneY)
+        {
+            planeY = minPlaneY;
+        }
+        if(planeY > maxPlaneY)
+        {
+            planeY = maxPlaneY;
+        }
+        planeSpeed = planeSpeed + 2;
+        if(touchStatus)
+        {
+            canvas.drawBitmap(resize_plane,planeX,planeY,null);
+            touchStatus = false;
+        }else
+        {
+            canvas.drawBitmap(resize_plane,planeX,planeY,null);
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            touchStatus = true;
+
+            planeSpeed = -22;
+        }
+
+        return super.onTouchEvent(event);
     }
 }
--- app/src/main/java/com/example/asm2/MainActivity.java
+++ app/src/main/java/com/example/asm2/MainActivity.java
@@ -3,15 +3,35 @@ package com.example.asm2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private FlyingPlaneView gameView;
+    private Handler handler = new Handler();
+    private final static long Interval = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gameView = new FlyingPlaneView(this);
         setContentView(gameView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        },0,Interval);
     }
 }
