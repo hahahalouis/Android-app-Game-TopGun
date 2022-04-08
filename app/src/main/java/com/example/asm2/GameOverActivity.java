c65d04d4d711cc2259c959d715e5981c9d2b11b4
wkh241 <wkahei34@gmail.com>
2022-04-08 19:22:04 +0800

    hiiii

--- app/src/main/java/com/example/asm2/GameOverActivity.java
+++ app/src/main/java/com/example/asm2/GameOverActivity.java
@@ -2,13 +2,35 @@ package com.example.asm2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class GameOverActivity extends AppCompatActivity {
-
+public class GameOverActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_retry, btn_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+            btn_retry = findViewById(R.id.btn_retry);
+            btn_menu = findViewById(R.id.btn_menu);
+
+            btn_retry.setOnClickListener(this);
+            btn_menu.setOnClickListener(this);
+        }
+
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_retry:
+                    Intent GameIntent = new Intent(getApplicationContext(), FlyingPlaneView.class);
+                    getApplicationContext().startActivity(GameIntent);
+                    break;
+
+                case R.id.btn_menu:
+
+                    break;
+            }
     }
 }
