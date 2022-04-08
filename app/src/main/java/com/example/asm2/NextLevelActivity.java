f3b4d26a720adcb2208979c77d57b189d902b4d7
wkh241 <wkahei34@gmail.com>
2022-04-08 18:23:37 +0800

    hiiii

---
+++ app/src/main/java/com/example/asm2/NextLevelActivity.java
@@ -0,0 +1,45 @@
+package com.example.asm2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class NextLevelActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_again, btn_next, btn_menu2;
+    ImageView star1, star2, star3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_level);
+        btn_again = findViewById(R.id.btn_again);
+        btn_next = findViewById(R.id.btn_next);
+        btn_menu2 = findViewById(R.id.btn_menu2);
+        star1 = findViewById(R.id.star1);
+        star2 = findViewById(R.id.star2);
+        star3 = findViewById(R.id.star3);
+        btn_next.setOnClickListener(this);
+        btn_again.setOnClickListener(this);
+        btn_menu2.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_again:
+                Intent GameIntent = new Intent(getApplicationContext(), FlyingPlaneView.class);
+                getApplicationContext().startActivity(GameIntent);
+                break;
+            case R.id.btn_next:
+                //Intent NextIntent = new Intent(getApplicationContext(), FlyingPlaneView.class);
+                //getApplicationContext().startActivity(NextIntent);
+                break;
+            case R.id.btn_menu2:
+
+                break;
+        }
+    }
+}
