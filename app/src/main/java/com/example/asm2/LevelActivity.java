4b2cfe35915de8518d664dc5d99addd6c269e520
pip2d07b <pip2d07b@gmail.com>
2022-04-09 17:20:51 +0800

    ken is handsome14

--- app/src/main/java/com/example/asm2/LevelActivity.java
+++ app/src/main/java/com/example/asm2/LevelActivity.java
@@ -26,41 +26,41 @@ public class LevelActivity extends AppCompatActivity implements View.OnClickList
             case R.id.tv_shop:
                 shopclicked();
                 break;
-                case R.id.textView:
-            case R.id.textView2:
-            case R.id.textView3:
-            case R.id.textView4:
-            case R.id.textView5:
-            case R.id.textView6:
-            case R.id.textView7:
-            case R.id.textView8:
-            case R.id.textView9:
-            case R.id.textView10:
-            case R.id.textView11:
-            case R.id.textView12:
-            case R.id.textView13:
-            case R.id.textView14:
-            case R.id.textView15:
-            case R.id.textView16:
-            case R.id.textView17:
-            case R.id.textView18:
-            case R.id.textView19:
-            case R.id.textView20:
-            case R.id.textView21:
-            case R.id.textView22:
-            case R.id.textView23:
-            case R.id.textView24:
-            case R.id.textView25:
-            case R.id.textView26:
-            case R.id.textView27:
-            case R.id.textView28:
-            case R.id.textView29:
-            case R.id.textView30:
-            case R.id.textView31:
-            case R.id.textView32:
-            case R.id.textView33:
-            case R.id.textView34:
-            case R.id.textView35:
+            case R.id.l1:
+            case R.id.l2:
+            case R.id.l3:
+            case R.id.l4:
+            case R.id.l5:
+            case R.id.l6:
+            case R.id.l7:
+            case R.id.l8:
+            case R.id.l9:
+            case R.id.l10:
+            case R.id.l11:
+            case R.id.l12:
+            case R.id.l13:
+            case R.id.l14:
+            case R.id.l15:
+            case R.id.l16:
+            case R.id.l17:
+            case R.id.l18:
+            case R.id.l19:
+            case R.id.l20:
+            case R.id.l21:
+            case R.id.l22:
+            case R.id.l23:
+            case R.id.l24:
+            case R.id.l25:
+            case R.id.l26:
+            case R.id.l27:
+            case R.id.l28:
+            case R.id.l29:
+            case R.id.l30:
+            case R.id.l31:
+            case R.id.l32:
+            case R.id.l33:
+            case R.id.l34:
+            case R.id.l35:
                 try {
                     TextView selected_level=findViewById(view.getId());
                     goLevel(Integer.parseInt(selected_level.getText().toString()));
@@ -87,41 +87,41 @@ public class LevelActivity extends AppCompatActivity implements View.OnClickList
     }
 
     public void initObject(){
-        l1=findViewById(R.id.textView);
-        l2=findViewById(R.id.textView2);
-        l3=findViewById(R.id.textView3);
-        l4=findViewById(R.id.textView4);
-        l5=findViewById(R.id.textView5);
-        l6=findViewById(R.id.textView6);
-        l7=findViewById(R.id.textView7);
-        l8=findViewById(R.id.textView8);
-        l9=findViewById(R.id.textView9);
-        l10=findViewById(R.id.textView10);
-        l11=findViewById(R.id.textView11);
-        l12=findViewById(R.id.textView12);
-        l13=findViewById(R.id.textView13);
-        l14=findViewById(R.id.textView14);
-        l15=findViewById(R.id.textView15);
-        l16=findViewById(R.id.textView16);
-        l17=findViewById(R.id.textView17);
-        l18=findViewById(R.id.textView18);
-        l19=findViewById(R.id.textView19);
-        l20=findViewById(R.id.textView20);
-        l21=findViewById(R.id.textView21);
-        l22=findViewById(R.id.textView22);
-        l23=findViewById(R.id.textView23);
-        l24=findViewById(R.id.textView24);
-        l25=findViewById(R.id.textView25);
-        l26=findViewById(R.id.textView26);
-        l27=findViewById(R.id.textView27);
-        l28=findViewById(R.id.textView28);
-        l29=findViewById(R.id.textView29);
-        l30=findViewById(R.id.textView30);
-        l31=findViewById(R.id.textView31);
-        l32=findViewById(R.id.textView32);
-        l33=findViewById(R.id.textView33);
-        l34=findViewById(R.id.textView34);
-        l35=findViewById(R.id.textView35);
+        l1=findViewById(R.id.l1);
+        l2=findViewById(R.id.l2);
+        l3=findViewById(R.id.l3);
+        l4=findViewById(R.id.l4);
+        l5=findViewById(R.id.l5);
+        l6=findViewById(R.id.l6);
+        l7=findViewById(R.id.l7);
+        l8=findViewById(R.id.l8);
+        l9=findViewById(R.id.l9);
+        l10=findViewById(R.id.l10);
+        l11=findViewById(R.id.l11);
+        l12=findViewById(R.id.l12);
+        l13=findViewById(R.id.l13);
+        l14=findViewById(R.id.l14);
+        l15=findViewById(R.id.l15);
+        l16=findViewById(R.id.l16);
+        l17=findViewById(R.id.l17);
+        l18=findViewById(R.id.l18);
+        l19=findViewById(R.id.l19);
+        l20=findViewById(R.id.l20);
+        l21=findViewById(R.id.l21);
+        l22=findViewById(R.id.l22);
+        l23=findViewById(R.id.l23);
+        l24=findViewById(R.id.l24);
+        l25=findViewById(R.id.l25);
+        l26=findViewById(R.id.l26);
+        l27=findViewById(R.id.l27);
+        l28=findViewById(R.id.l28);
+        l29=findViewById(R.id.l29);
+        l30=findViewById(R.id.l30);
+        l31=findViewById(R.id.l31);
+        l32=findViewById(R.id.l32);
+        l33=findViewById(R.id.l33);
+        l34=findViewById(R.id.l34);
+        l35=findViewById(R.id.l35);
 
         btn_shop=findViewById(R.id.tv_shop);
 
